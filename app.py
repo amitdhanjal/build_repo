10aa2d6142d10f666b26227d44a972110b89a0c3
Amit <amitdhanjal43@gmail.com>
2020-03-18 12:25:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import pymongo
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Hi,I am running on Host and testing the triggger"
+
+if __name__ == '__main__':
+	app.run(debug=True,port=8080,'0.0.0.0')
